1c31a1afac72520cafe72377cf00fba8ef184153
Bangyen Pham <66754151+bangyen@users.noreply.github.com>
2020-07-07 01:31:26 -0400

    Create ascii-art.py

---
+++ cell-based/ascii-art.py
@@ -0,0 +1,29 @@
+import sys
+
+if __name__ == '__main__':
+  with open(sys.argv[1]) as file:
+    code = file.readlines()
+  
+  commands = {
+    '\': 
+  }
+  
+  extra = 0
+  period = False
+  bracket = False
+  
+  for line in code:
+    if extra:
+      extra -= 1
+      continue
+    if bracket:
+      if line[0] == '|':
+        ...
+      else:
+        ...
+    if period:
+      if line[0] == '_':
+        ...
+      else:
+        ...
+    
