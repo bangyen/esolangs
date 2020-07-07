7f976a719a051e4dde2c6d6f33acd7ac8be832a5
Bangyen Pham <66754151+bangyen@users.noreply.github.com>
2020-07-07 03:48:19 -0400

    Update ascii-art.py

--- cell-based/ascii-art.py
+++ cell-based/ascii-art.py
@@ -1,28 +1,50 @@
 import sys
+import re
+
+
+def find_bracket(symbols, num, mode):
+    bracket = (-1) ** (mode == ']')
+    while bracket:
+        num = num + (-1) ** (mode == ']')
+        if (symb := symbols[num]) in '[]':
+            bracket += (-1) ** (symb == ']')
+    return num
+
 
 if __name__ == '__main__':
     with open(sys.argv[1]) as file:
-        code = file.readlines()
+        code = ''
+        for line in file:
+            code += re.sub(' *\n', '\n', line)
+        code = code.split('\n\n')
 
-    commands = {
-        '\\': 1
-    }
+    sym_dict = [
+        {'-': '-'}, {'#': '.'}, {'|': ','}, {'\\': '<', '/': '>'},
+        {'|': '+'}, {'_': '[', '|': ']'}
+    ]
 
-    extra = 0
-    period = False
-    bracket = False
+    index = pointer = 0
+    output = bf = ''
+    cells = [0]
 
-    for line in code:
-        if extra:
-            extra -= 1
-            continue
-        if bracket:
-            if line[0] == '|':
-                ...
-            else:
-                ...
-        if period:
-            if line[0] == '_':
-                ...
-            else:
-                ...
+    for sym in code:
+        bf += sym_dict[sym.count('\n')][sym[-1]]
+    while index < len(bf):
+        char = bf[index]
+        if char in '<>':
+            pointer = max(pointer + (-1) ** (char == '<'), 0)
+            if pointer > len(cells) - 1:
+                cells.append(0)
+        elif char in '+-':
+            cells[pointer] = (cells[pointer] + (-1) ** (char == '-')) % 256
+        elif char == '.':
+            if not cells[pointer]:
+                break
+            output += chr(cells[pointer])
+        elif char == ',':
+            cells[pointer] = ord((input('Input: ') + chr(0))[0])
+        elif char in '[]':
+            if ((cells[pointer] != 0) + (char == '[')) % 2:
+                index = find_bracket(bf, index, char) - 1
+        index += 1
+    print(output)
