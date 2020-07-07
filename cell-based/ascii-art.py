8d165f8d8446d6fb4530247172854404ed6efa78
Bangyen Pham <66754151+bangyen@users.noreply.github.com>
2020-07-07 01:57:37 -0400

    Update ascii-art.py

--- cell-based/ascii-art.py
+++ cell-based/ascii-art.py
@@ -1,29 +1,28 @@
 import sys
 
 if __name__ == '__main__':
-  with open(sys.argv[1]) as file:
-    code = file.readlines()
-  
-  commands = {
-    '\': 
-  }
-  
-  extra = 0
-  period = False
-  bracket = False
-  
-  for line in code:
-    if extra:
-      extra -= 1
-      continue
-    if bracket:
-      if line[0] == '|':
-        ...
-      else:
-        ...
-    if period:
-      if line[0] == '_':
-        ...
-      else:
-        ...
-    
+    with open(sys.argv[1]) as file:
+        code = file.readlines()
+
+    commands = {
+        '\\': 1
+    }
+
+    extra = 0
+    period = False
+    bracket = False
+
+    for line in code:
+        if extra:
+            extra -= 1
+            continue
+        if bracket:
+            if line[0] == '|':
+                ...
+            else:
+                ...
+        if period:
+            if line[0] == '_':
+                ...
+            else:
+                ...
