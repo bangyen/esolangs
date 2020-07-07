ccec9d6d2b0b4ef0fea05fd68f90502070800e36
Bangyen Pham <66754151+bangyen@users.noreply.github.com>
2020-07-07 14:11:09 -0400

    Update ascii-art.py

--- cell-based/ascii-art.py
+++ cell-based/ascii-art.py
@@ -16,34 +16,34 @@ if __name__ == '__main__':
         code = ''
         for line in file:
             code += re.sub(' *\n', '\n', line)
-        code = code.split('\n\n')
+        code = [line for line in code.split('\n\n') if line.count('\n') < 6]
 
+    index = pointer = 0
+    output = bf = ''
+    cells = [0]
     sym_dict = [
         {'-': '-'}, {'#': '.'}, {'|': ','}, {'\\': '<', '/': '>'},
         {'|': '+'}, {'_': '[', '|': ']'}
     ]
 
-    index = pointer = 0
-    output = bf = ''
-    cells = [0]
-
     for sym in code:
-        bf += sym_dict[sym.count('\n')][sym[-1]]
+        try:
+            bf += sym_dict[sym.count('\n')][sym[-1]]
+        except KeyError as k:
+            pass
     while index < len(bf):
         char = bf[index]
         if char in '<>':
             pointer = max(pointer + (-1) ** (char == '<'), 0)
-            if pointer > len(cells) - 1:
+            if char == '>' and pointer > len(cells) - 1:
                 cells.append(0)
         elif char in '+-':
             cells[pointer] = (cells[pointer] + (-1) ** (char == '-')) % 256
         elif char == '.':
-            if not cells[pointer]:
-                break
             output += chr(cells[pointer])
         elif char == ',':
             cells[pointer] = ord((input('Input: ') + chr(0))[0])
-        elif char in '[]':
+        else:
             if ((cells[pointer] != 0) + (char == '[')) % 2:
                 index = find_bracket(bf, index, char) - 1
         index += 1
