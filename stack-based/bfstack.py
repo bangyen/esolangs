18a9ef5282b357d1ac9e1b927a7d8ca030fa5023
Bangyen Pham <66754151+bangyen@users.noreply.github.com>
2020-07-08 18:44:50 -0400

    Update bfstack.py

--- stack-based/bfstack.py
+++ stack-based/bfstack.py
@@ -1,32 +1,22 @@
-import sys
+code = open(__import__('sys').argv[1]).read()
 
+line = []
+stack = [0]
+pointer = 0
 
-def main():
-    with open(sys.argv[1]) as file:
-        code = file.read()
-    line = []
-    stack = [0]
-    pointer = 0
-    sym_dict = {
-        '>': lambda: stack.append(0),
-        '<': lambda: stack.pop(-1) if stack else 0,
-        '+': lambda: stack.append((stack.pop(-1) + 1) % 256),
-        '-': lambda: stack.append((stack.pop(-1) - 1) % 256),
-        '.': lambda: print(chr(stack[-1]), end=''),
-        ',': lambda: stack.append(ord((input('\n' * bool(line) + 'Input: ') + chr(0))[0])) or line.append(0)
-    }
-    while pointer < len(code):
-        if (char := code[pointer]) in sym_dict:
-            sym_dict[char]()
-        elif char in '[]':
-            if (stack[-1] != 0) - (char == '['):
-                bracket = (-1, 1)[char == '[']
-                while bracket:
-                    pointer += (-1, 1)[char == '[']
-                    if code[pointer] in '[]':
-                        bracket += (-1, 1)[code[pointer] == '[']
-        pointer += 1
+sym_dict = {
+    '>': lambda: stack.append(0),
+    '<': lambda: stack.pop(-1) if stack else 0,
+    '+': lambda: stack.append((stack.pop(-1) + 1) % 256),
+    '-': lambda: stack.append((stack.pop(-1) - 1) % 256),
+    '.': lambda: print(chr(stack[-1]), end=''),
+    ',': lambda: stack.append(ord((input('\n' * bool(line) + 'Input: ') + chr(0))[0])) or line.append(0)
+}
 
-
-if __name__ == '__main__':
-    main()
+while pointer < len(code):
+    x = sym_dict[char]() if (char := code[pointer]) in sym_dict else 0
+    bracket = (-1, 1)[char == '['] if char in '[]' and (stack[-1] != 0) - (char == '[') else 0
+    while bracket:
+        pointer += (-1, 1)[char == '[']
+        bracket += (-1, 1)[code[pointer] == '['] * (code[pointer] in '[]')
+    pointer += 1
