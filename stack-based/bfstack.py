6cbb1eeb6305b4cfc16ef28874207bb38a395acf
Bangyen Pham <66754151+bangyen@users.noreply.github.com>
2020-07-08 17:23:03 -0400

    Create bfstack.py

---
+++ stack-based/bfstack.py
@@ -0,0 +1,30 @@
+import sys
+import re
+
+def main():
+  code = open(sys.argsv[0]).read()
+  line = []
+  stack = []
+  pointer = 0
+  sym_dict = {
+    '>': lambda: stack.append(0),
+    '<': lambda: stack.pop(-1) if stack else 0,
+    '+': lambda: stack[-1] = (stack[-1] + 1) % 256,
+    '-': lambda: stack[-1] -= (stack[-1] - 1) % 256,
+    '.': lambda: print(chr(stack[-1), end = ''),
+    ',': lambda: stack.append(input('\nInput: '))
+         if line else stack.append(input('Input: ')) and line.append(0)
+  }
+  while pointer < len(code):
+    if (char := code[pointer]) in sym_dict:
+      sym_dict[char]()
+    elif char in '[]':
+      bracket = (-1, 1)[char == '[']
+      while bracket:
+        pointer += (-1, 1)[char == '[']
+        if code[pointer] in '[]':
+          bracket += (-1, 1)[char == '[']
+    pointer += 1
+
+if __name__ == '__main__':
+  main()
