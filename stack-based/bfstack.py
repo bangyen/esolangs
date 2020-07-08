52abcc347bafa171fed605b3922de2bf3081ec06
Bangyen Pham <66754151+bangyen@users.noreply.github.com>
2020-07-08 17:58:54 -0400

    Update bfstack.py

--- stack-based/bfstack.py
+++ stack-based/bfstack.py
@@ -2,7 +2,8 @@ import sys
 
 
 def main():
-    code = open(sys.argv[0]).read()
+    with open(sys.argv[1]) as file:
+        code = file.read()
     line = []
     stack = [0]
     pointer = 0
@@ -11,19 +12,20 @@ def main():
         '<': lambda: stack.pop(-1) if stack else 0,
         '+': lambda: stack.append((stack.pop(-1) + 1) % 256),
         '-': lambda: stack.append((stack.pop(-1) - 1) % 256),
-        '.': lambda: print(chr(stack[-1]), end = ''),
-        ',': lambda: stack.append(input('\nInput: ')) if line else stack.append(input('Input: ')) and line.append(0)
+        '.': lambda: print(chr(stack[-1]), end=''),
+        ',': lambda: stack.append(ord((input('\n' * bool(line) + 'Input: ') + chr(0))[0])) or line.append(0)
     }
     while pointer < len(code):
         if (char := code[pointer]) in sym_dict:
             sym_dict[char]()
         elif char in '[]':
-            bracket = (-1, 1)[char == '[']
-            while bracket:
-                pointer += (-1, 1)[char == '[']
-                if code[pointer] in '[]':
-                    bracket += (-1, 1)[char == '[']
-    pointer += 1
+            if (stack[-1] != 0) - (char == '['):
+                bracket = (-1, 1)[char == '[']
+                while bracket:
+                    pointer += (-1, 1)[char == '[']
+                    if code[pointer] in '[]':
+                        bracket += (-1, 1)[code[pointer] == '[']
+        pointer += 1
 
 
 if __name__ == '__main__':
