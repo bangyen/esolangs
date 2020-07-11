3048139c4d3ac27eaadf84ee6d62bd46c2cc4788
Bangyen Pham <66754151+bangyen@users.noreply.github.com>
2020-07-11 18:10:03 -0400

    Update back.py

--- cell-based/back.py
+++ cell-based/back.py
@@ -1,23 +1,20 @@
-velocity = (0, 1)
 pointer = cond = char = 0
-instruct = (cells := [0]) * 2
+inst = (cells := [0]) * 2
+vel = (0, 1)
 
 code = [list(line) for line in open(__import__('sys').argv[1]).readlines()]
 code = [line + [' '] * (max(len(lst) for lst in code) - len(line)) for line in code]
 
-while '*' in sum(code, []) and (char := code[instruct[0]][instruct[1]]) != '*':
+while '*' in sum(code, []) and (char := code[inst[0]][inst[1]]) != '*':
     if char in '\\/':
-        velocity = [k * (-1, 1)[char == '\\'] for k in (velocity[1], velocity[0])]
+        vel = [k * (-1, 1)[char == '\\'] for k in (vel[1], vel[0])]
     if char in '<>':
         pointer += (-1, 1)[char == '>']
         if not 0 <= pointer < len(cells):
-            if char == '>':
-                cells.append(0)
-            else:
-                cells.insert(0, 0)
-                pointer = max(0, pointer)
+            funcs = [lambda n: cells.insert(0, n), cells.append]
+            funcs[char == '>'](0)
+            pointer = max(0, pointer)
     cells[pointer] = (cells[pointer] + (char == '-')) % 2
-    cond = (not cells[pointer]) * (char == '+')
     for k in (0, 1):
-        instruct[k] += velocity[k] * (cond + 1)
-        instruct[k] = instruct[k] % len((code, code[0])[k])
+        inst[k] += vel[k] * ((not cells[pointer]) * (char == '+') + 1)
+        inst[k] = inst[k] % len((code, code[0])[k])
