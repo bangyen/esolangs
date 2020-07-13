b1f1f51d0814efbed4ab0680e52415bef4f23cfc
Bangyen Pham <66754151+bangyen@users.noreply.github.com>
2020-07-13 15:19:36 -0400

    Create bitdeque.py

---
+++ deque-based/bitdeque.py
@@ -0,0 +1,22 @@
+import sys
+
+deque = []
+point = reg = num = 0
+code = open(sys.argv[1]).read().split()
+
+sym_dict = {
+    'PUSH': lambda: deque.append(reg),
+    'INJECT': lambda: deque.insert(0, reg),
+    'POP': lambda r: deque.pop(-1) if deque else 0,
+    'EJECT': lambda r: deque.pop(0) if deque else 0,
+    'INVERT': lambda r: not reg
+}
+
+while point < len(code):
+    if (sym := code[point]) in sym_dict:
+        count = (func := sym_dict[sym]).__code__.co_argcount
+        reg = func(reg) if count else func() or reg
+    elif sym == 'GOTO':
+        if point < len(code) - 1 and (num := code[point + 1]).isdigit():
+            pointer = max(-1, int(num) - 2)
+    point += 1
