b4d9052081ad76be06c478557c131362e1d45995
Bangyen Pham <66754151+bangyen@users.noreply.github.com>
2020-07-13 15:29:00 -0400

    Update bitdeque.py

--- deque-based/bitdeque.py
+++ deque-based/bitdeque.py
@@ -15,7 +15,7 @@ sym_dict = {
 while point < len(code):
     if (sym := code[point]) in sym_dict:
         count = (func := sym_dict[sym]).__code__.co_argcount
-        reg = func(reg) if count else func() or reg
+        reg = func(reg) if count else (func() or reg)
     elif sym == 'GOTO':
         if point < len(code) - 1 and (num := code[point + 1]).isdigit():
             point = max(-1, int(num) - 2) if reg else point
