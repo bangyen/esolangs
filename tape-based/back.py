e918293a41ff6b26ec7ea66d86178762a3303b65
Bangyen Pham <66754151+bangyen@users.noreply.github.com>
2020-07-19 01:04:51 -0400

    Update back.py

--- tape-based/back.py
+++ tape-based/back.py
@@ -1,30 +1,37 @@
-file = open(__import__('sys').argv[1]).readlines()
+import sys
 
-point = cond = char = (vel := (0, 1))[0]
-inst = (cells := [0]) * 2
 
-funcs = [lambda n: cells.insert(0, n), cells.append]
-code = [
-    line + ' ' * (max(map(len, file)) - len(line))
-    for line in file
-]
+def add(pos, move, sym):
+    x = (pos[0] + move[0]) % len(sym)
+    y = (pos[1] + move[1]) % len(sym[0])
+    return [x, y]
 
-while '*' in sum(code, []):
-    char = code[inst[0]][inst[1]]
-    if char == '*':
-        break
-    elif char in '\\/':
-        mul = -1 ** (char == '/')
-        vel = [k * mul for k in vel][::-1]
-    elif char in '<>':
-        point += -1 ** (char == '<')
-        if not 0 <= point < len(cells):
-            funcs[char == '>'](0)
-            point = max(0, point)
-    elif char == '-':
-        cells[point] = not cells[point]
-    mul = (not cells[point]) * (char == '+') + 1
-    inst = [
-        (inst[k] + vel[k] * mul)
-        % len([code, code[0]][k]) for k in [0, 1]
+
+if __name__ == '__main__':
+    funcs = [lambda n: cells.insert(0, n), cells.append]
+    point = cond = char = (vel := (0, 1))[0]
+    inst = (cells := [0]) * 2
+    
+    with open(sys.argv[1]).readlines() as file:
+    code = [
+        line + ' ' * (max(map(len, file)) - len(line))
+        for line in file
     ]
+    
+    while '*' in sum(code, []):
+        char = code[inst[0]][inst[1]]
+        if char == '*':
+            break
+        elif char in '\\/':
+            mul = -1 ** (char == '/')
+            vel = [k * mul for k in vel][::-1]
+        elif char in '<>':
+            point += -1 ** (char == '<')
+            if not 0 <= point < len(cells):
+                funcs[char == '>'](0)
+                point = max(0, point)
+        elif char == '-':
+            cells[point] = not cells[point]
+        elif char == '+':
+            inst = add(inst, vel, code)
+        inst = add(inst, vel, code)
