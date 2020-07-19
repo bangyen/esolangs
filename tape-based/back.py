0b8c5c49009a88edc1f36ac2eddfb5ede2a4e2c1
Bangyen Pham <66754151+bangyen@users.noreply.github.com>
2020-07-19 00:13:54 -0400

    Update back.py

--- tape-based/back.py
+++ tape-based/back.py
@@ -1,22 +1,30 @@
-from sys import argv
+file = open(__import__('sys').argv[1]).readlines()
 
-pointer = cond = char = 0
+point = cond = char = (vel := (0, 1))[0]
 inst = (cells := [0]) * 2
-vel = (0, 1)
 
-code = [list(line) for line in open(argv[1]).readlines()]
-code = [line + [' '] * (max(len(lst) for lst in code) - len(line)) for line in code]
 funcs = [lambda n: cells.insert(0, n), cells.append]
+code = [
+    list(line) + [' '] * (max(map(len, file)) - len(line))
+    for line in file
+]
 
-while '*' in sum(code, []) and (char := code[inst[0]][inst[1]]) != '*':
-    if char in '\\/':
-        vel = [k * (-1, 1)[char == '\\'] for k in (vel[1], vel[0])]
+while '*' in sum(code, []):
+    char = code[inst[0]][inst[1]]
+    if char == '*':
+        break
+    elif char in '\\/':
+        mul = -1 ** (char == '/')
+        vel = [k * mul for k in vel][::-1]
     elif char in '<>':
-        pointer += (-1, 1)[char == '>']
-        if not 0 <= pointer < len(cells):
+        point += -1 ** (char == '<')
+        if not 0 <= point < len(cells):
             funcs[char == '>'](0)
-            pointer = max(0, pointer)
-    cells[pointer] = (cells[pointer] + (char == '-')) % 2
-    for k in (0, 1):
-        inst[k] += vel[k] * ((not cells[pointer]) * (char == '+') + 1)
-        inst[k] = inst[k] % len((code, code[0])[k])
+            point = max(0, point)
+    elif char == '-':
+        cells[point] = not cells[point]
+    mul = (not cells[point]) * (char == '+') + 1
+    inst = [
+        (inst[k] + vel[k] * mul)
+        % len([code, code[0]][k]) for k in [0, 1]
+    ]
