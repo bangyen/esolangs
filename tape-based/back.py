6d3c268413f1a477cb4a7f88bbf7339cfb70a123
Bangyen Pham <66754151+bangyen@users.noreply.github.com>
2020-07-18 22:47:51 -0400

    Update back.py

--- tape-based/back.py
+++ tape-based/back.py
@@ -1,17 +1,19 @@
+from sys import argv
+
 pointer = cond = char = 0
 inst = (cells := [0]) * 2
 vel = (0, 1)
 
-code = [list(line) for line in open(__import__('sys').argv[1]).readlines()]
+code = [list(line) for line in open(argv[1]).readlines()]
 code = [line + [' '] * (max(len(lst) for lst in code) - len(line)) for line in code]
+funcs = [lambda n: cells.insert(0, n), cells.append]
 
 while '*' in sum(code, []) and (char := code[inst[0]][inst[1]]) != '*':
     if char in '\\/':
         vel = [k * (-1, 1)[char == '\\'] for k in (vel[1], vel[0])]
-    if char in '<>':
+    elif char in '<>':
         pointer += (-1, 1)[char == '>']
         if not 0 <= pointer < len(cells):
-            funcs = [lambda n: cells.insert(0, n), cells.append]
             funcs[char == '>'](0)
             pointer = max(0, pointer)
     cells[pointer] = (cells[pointer] + (char == '-')) % 2
