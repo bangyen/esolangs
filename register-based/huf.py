16e99ee60ab09bc33fc34b3e68e783b02a588319
Bangyen Pham <66754151+bangyen@users.noreply.github.com>
2020-07-19 00:29:15 -0400

    Update huf.py

--- register-based/huf.py
+++ register-based/huf.py
@@ -3,17 +3,8 @@ import sys
 import re
 
 
-def error(pointer, clause):
-    err_dict = {
-        'M#': "Missing '#'", 'US': 'Unrecognized symbol'
-    }
-    if clause in err_dict:
-        print(f"At MARK_>{pointer}< {err_dict[clause]}")
-
-
 def interpret(code):
-    pointer = temp = 0
-    output = False
+    pointer = temp = output = 0
     while pointer < len(code):
         sym = code[pointer]
         if sym in ['#', '@']:
@@ -21,8 +12,6 @@ def interpret(code):
         elif sym == '>':
             if output:
                 print(chr(temp), end='')
-            else:
-                error(pointer, "M#")
             temp = 0
         elif sym == '|':
             num = code[pointer + 1]
