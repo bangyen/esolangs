3917042ef1bdc139f01a9f9f60c946c83abe0762
Bangyen Pham <66754151+bangyen@users.noreply.github.com>
2020-07-28 03:11:17 -0400

    Create excon.py

---
+++ tape-based/excon.py
@@ -0,0 +1,15 @@
+import sys
+
+pool, cell = ([0] * 8, 7)
+code = open(sys.argv[1]).read()
+
+for sym in code:
+    if sym == ':':
+        pool, cell = ([0] * 8, 7)
+    elif sym == '^':
+        pool[cell] = (not pool[cell]) + 0
+    elif sym == '!':
+        num = ''.join(map(str, pool))
+        print(chr(int(num, 2)), end='')
+    elif sym == '<':
+        cell -= 1
