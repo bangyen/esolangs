5058be8b83e22cd7c03450c11ded033a49d14a96
Bangyen Pham <66754151+bangyen@users.noreply.github.com>
2021-05-18 22:06:21 -0400

    Update generate.py

--- miscellaneous/generate.py
+++ miscellaneous/generate.py
@@ -26,3 +26,30 @@ def bfstack(text):
         acc = ord(c)
 
     return res
+
+def container(text):
+    ins = ''
+    ind = last = 0
+
+    for c in text:
+        if (o := ord(c) - last) >= 0:
+            ins += f'+{o} A>={ind}\n' \
+                   + f'-{o} A>={ind + 1}\n'
+        else:
+            ins += f'-{-o} A>={ind}\n' \
+                   + f'+{-o} A>={ind + 1}\n'
+        last = ord(c)
+        ind += 2
+
+    res = 'A:\n' \
+        + '+1 EXIT>=1\n\n' \
+        + 'PRINT:\n' \
+        + '+1 PRINT<=0\n' \
+        + '-1 PRINT>=1\n\n' \
+        + 'OUT:\n' \
+        + f'{ins}\n' \
+        + 'EXIT=1:\n' \
+        + '-1 A>=' \
+        + str(2 * len(text) - 2)
+
+    return res
