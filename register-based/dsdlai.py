e55d4f33a8d75e07c1fc2d29e2f5fe18a827ed90
Bangyen Pham <66754151+bangyen@users.noreply.github.com>
2021-06-21 18:23:20 -0400

    Update dsdlai.py

--- register-based/dsdlai.py
+++ register-based/dsdlai.py
@@ -1,1 +1,22 @@
-__import__('dig').main(True)
+import random
+import sys
+import dig
+
+
+def rand():
+    num = random.randint(20, 90)
+
+    def chance():
+        n = random.randint(1, 100)
+        if n <= num:
+            print('\nYou died.')
+        return n <= num
+    return chance
+
+
+if __name__ == '__main__':
+    f = open(sys.argv[1])
+    data = f.readlines()
+    f.close()
+
+    dig.run(data, rand())
