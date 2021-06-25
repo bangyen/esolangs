a82b4cf2a6d0cbb9dbcde04ebb85fb954c3d8e77
Bangyen Pham <66754151+bangyen@users.noreply.github.com>
2021-06-25 04:21:02 -0400

    Update ascii-art.py

--- tape-based/ascii-art.py
+++ tape-based/ascii-art.py
@@ -2,50 +2,67 @@ import sys
 import re
 
 
-def find_bracket(symbols, num, mode):
-    bracket = (1, -1)[mode == ']']
-    while bracket:
-        num = num + (1, -1)[mode == ']']
-        if (symb := symbols[num]) in '[]':
-            bracket += (1, -1)[symb == ']']
-    return num
+def run(code):
+    code = re.sub(' +\n', '\n', code)
+    code = code.split('\n\n')
+
+    bf = ''
+    stk = []
+    tape = [0]
+    ind = ptr = 0
+    new = False
+
+    sym = {
+        (0, '-'): '-', (1, '#'): '.',
+        (2, '|'): ',', (3, '\\'): '<',
+        (3, '/'): '>', (4, '|'): '+',
+        (5, '_'): '[', (5, '|'): ']'
+    }
+
+    for c in code:
+        t = (c.count('\n'), c[-1])
+        if t in sym:
+            bf += sym[t]
+
+    while ind < len(bf):
+        if (char := bf[ind]) == '>':
+            ptr += 1
+            if ptr == len(tape):
+                tape.append(0)
+        elif char == '<' and ptr:
+            ptr -= 1
+        elif char in '+':
+            tape[ptr] = (tape[ptr] + 1) % 256
+        elif char == '-':
+            tape[ptr] = (tape[ptr] - 1) % 256
+        elif char == '.':
+            print(chr(tape[ptr]), end='')
+            new = True
+        elif char == ',':
+            val = input('\n' * new + 'Input: ')
+            tape[ptr] = ord(val[0])
+            new = False
+        elif char == '[':
+            if tape[ptr]:
+                stk.append(ind)
+            else:
+                match = 1
+                while match:
+                    ind += 1
+                    if ind == len(bf):
+                        return
+                    elif bf[ind] == '[':
+                        match += 1
+                    elif bf[ind] == ']':
+                        match -= 1
+        elif char == ']':
+            ind = stk.pop() - 1
+
+        ind += 1
 
 
 if __name__ == '__main__':
-    with open(sys.argv[1]) as file:
-        code = ''
-        for line in file:
-            code += re.sub(' *\n', '\n', line)
-        code = [line for line in code.split('\n\n') if line.count('\n') < 6]
-
-    bf = ''
-    cells = [0]
-    index = pointer = new = 0
-    
-    sym_dict = [
-        {'-': '-'}, {'#': '.'}, {'|': ','}, {'\\': '<', '/': '>'},
-        {'|': '+'}, {'_': '[', '|': ']'}
-    ]
-
-    for sym in code:
-        try:
-            bf += sym_dict[sym.count('\n')][sym[-1]]
-        except KeyError as k:
-            pass
-    while index < len(bf):
-        char = bf[index]
-        if char in '<>':
-            pointer = max(pointer + (1, -1)[char == '<'], 0)
-            if char == '>' and pointer > len(cells) - 1:
-                cells.append(0)
-        elif char in '+-':
-            cells[pointer] = (cells[pointer] + (1, -1)[char == '-']) % 256
-        elif char == '.':
-            print(chr(cells[pointer]), end='')
-        elif char == ',':
-            cells[pointer] = ord((input('\n' * new + 'Input: ') + chr(0))[0])
-            new = True
-        else:
-            if (cells[pointer] != 0) - (char == '['):
-                index = find_bracket(bf, index, char) - 1
-        index += 1
+    if len(sys.argv) > 1:
+        with open(sys.argv[1]) as file:
+            data = file.read()
+            run(data)
