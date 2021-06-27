5363594093009c3c9074303e42b2ab54cba62108
Bangyen Pham <66754151+bangyen@users.noreply.github.com>
2021-06-27 19:49:05 -0400

    Update RAM0.c

--- compilers/c/RAM0.c
+++ compilers/c/RAM0.c
@@ -2,99 +2,89 @@
 #include <stdlib.h>
 #include <string.h>
 
-int   max, loop, ind;
-char  jump[5] = {'G'};
-char* string, ch;
-
-char* digits(FILE *symbols) {
-    char* num = "";
-    char* temp;
-
-    while (1) {
-        int len = 0;
-        char sym = getc(symbols);
-        if (isdigit(sym)) {
-            if (len) {
-                temp = calloc(len + 1, 1);
-                sprintf(temp, "%s%c", num, sym);
-                num = temp;
-            } else {
-                num = calloc(1, 1);
-                num[0] = sym;
-            }
-            len++;
-        } else {
-            if (len) {
-                temp = calloc(len + 11, 1);
-                sprintf(temp, "ind = %s - 1;", num);
-                num = temp;
-            } else if (sym == ' ')
-                continue;
-            fseek(symbols, -1, SEEK_CUR);
-            break;
-        }
-    }
-
-    return num;
-}
-
 int main(int argc, char *argv[]) {
-    FILE *file = fopen(argv[1], "r");
+    FILE *file   = fopen(argv[1], "r");
     FILE *output = fopen("output.c", "w");
 
+    int  max = 0, ind = 0, num = 0;
+    char jump[5] = {'G'};
+    char *str, ch;
+
     while ((ch = getc(file)) != EOF) {
-        loop += (ch == 'G') || (ch == 'C');
+        num |= ch == 'G';
         max += ch == 'A';
     }
 
     rewind(file);
     fprintf(
         output,
-        "#include <stdio.h>\n\nint z, n, %send;\n"
-        "int ram[%d];\n\nint main()\n{\n",
-        loop ? "ind, " : "", max + 1
-    );
-    if (loop)
+        "#include <stdio.h>\n\nint z, n%s;"
+        "\nint ram[%d];\n\nint main()\n{\n",
+        num ? ", ind" : "", max);
+    if (num)
         fputs(
-            "\twhile (!end)\n\t{\n\t\tind++;\n"
-            "\t\tswitch (ind)\n\t\t{\n", output
-        );
-    
+            "\twhile (++ind)\n\t{\n\t\t"
+            "switch (ind)\n\t\t{\n", output);
+    max = 1;
+
     while ((ch = getc(file)) != EOF) {
-        ind++; string = "";
+        ind++; str = "";
         switch (ch) {
-            case 'Z':  string = "z = 0;"; break;
-            case 'A':  string = "z++;"; break;
-            case 'N':  string = "n = z;"; break;
-            case 'C':  string = "ind += z == 0;"; break;
-            case 'L':  string = "z = ram[z];"; break;
-            case 'S':  string = "ram[n] = z;"; break;
+            case 'Z': str = "z = 0;"; break;
+            case 'A': str = "z++;"; break;
+            case 'N': str = "n = z;"; break;
+            case 'L': str = "z = ram[z];"; break;
+            case 'S': str = "ram[n] = z;"; break;
+            case 'C':
+                str = num
+                    ? "ind += z == 0;"
+                    : "if (z)";
+                break;
             case 'G':
-                jump[1] = getc(file);
-                jump[2] = getc(file);
-                jump[3] = getc(file);
-                if (!strcmp(jump, "GOTO"))
-                    string = digits(file);
-                else fseek(file, -3, SEEK_CUR);
+                for (max = 1; max < 4; max++)
+                    jump[max] = getc(file);
+                if (!strcmp(jump, "GOTO")) {
+                    int val;
+
+                    fscanf(file, "%d", &val);
+                    int len = snprintf(
+                        NULL, 0, "%d", val);
+                    str = malloc(len + 12);
+                    snprintf(
+                        str, len + 12,
+                        "ind = %i - 1;", val);
+                } else {
+                    fseek(file, -3, SEEK_CUR);
+                }
                 break;
         }
-        
-        if (strcmp(string, "")) {
-            if (loop) {
-                fprintf(output, "\t\t\tcase %d: ", ind);
-                fprintf(output, "%s break;\n", string);
-            } else
-                fprintf(output, "\t%s\n", string);
-        } else ind--;
+
+        if (strcmp(str, "")) {
+            if (num) {
+                fprintf(
+                    output, "\t\t\tcase %d: "
+                    "%s break;\n", ind, str);
+            } else {
+                int val;
+
+                for (val = 0; val < max; val++)
+                    fputc('\t', output);
+
+                ch == 'C' ? max++ : (max = 1);
+                fprintf(output, "%s\n", str);
+            }
+        } else {
+            ind--;
+        }
     }
-    string =
-        "\t\t\tdefault: end = 1;"
-        " break;\n\t\t}\n\t}\n";
+
+    str = "\t\t\tdefault: ind = -1;"
+          " break;\n\t\t}\n\t}\n";
     fprintf(
-        output, "%s\treturn 0;\n}",
-        loop ? string : ""
-    );
-    
+        output, "%s\n\tprintf(\"Z: %s\\nN:"
+        " %s\\n\", z, n);\n\treturn 0;\n}",
+        num ? str : "", "%d", "%d");
+
     fclose(file);
     fclose(output);
 
