40da50160ffb41d17329f254bb6bf95bb78967f6
bangyen <bangyen99@gmail.com>
2021-06-28 15:40:14 -0400

    cleaned up bfstack interpreter

--- stack-based/bfstack.py
+++ stack-based/bfstack.py
@@ -1,25 +1,49 @@
-code = open(__import__('sys').argv[1]).read()
+import sys
 
-line = [1]
-stack = [0]
-pointer = 0
 
-sym_dict = {
-    '>': lambda: stack.append(0),
-    '<': lambda: stack.pop(-1) if stack else 0,
-    '+': lambda: stack.append((stack.pop(-1) + 1) % 256),
-    '-': lambda: stack.append((stack.pop(-1) - 1) % 256),
-    '.': lambda: print(chr(stack[-1]), end='') or line.append(0),
-    ',': lambda: stack.append(ord((input('\nInput: '[line[-1]:]) + '\0')[0]))
-}
+def run(code):
+    stk = []
+    lst = []
+    new = 1
+    ind = 0
 
-while pointer < len(code):
-    char = code[pointer]
-    x = sym_dict[char]() if char in sym_dict else 0
-    cond = char in '[]' and (stack[-1] != 0) - (char == '[')
-    bracket = (-1, 1)[char == '['] if cond else 0
-    while bracket:
-        pointer += (-1, 1)[char == '[']
-        bracket += (-1, 1)[code[pointer] == '['] \
-            * (code[pointer] in '[]')
-    pointer += 1
+    while ind < len(code):
+        if (char := code[ind]) == '>':
+            stk.append(0)
+        elif char == '<':
+            stk.pop()
+        elif char == '+':
+            stk[-1] = (stk[-1] + 1) % 256
+        elif char == '-':
+            stk[-1] = (stk[-1] - 1) % 256
+        elif char == '.':
+            print(chr(stk[-1]), end='')
+            new = 0
+        elif char == ',':
+            val = input('\nInput: '[new:])
+            stk.append(ord(val[0]))
+            new = 1
+        elif char == '[':
+            if stk[-1]:
+                lst.append(ind)
+            else:
+                match = 1
+                while match:
+                    ind += 1
+                    if ind == len(code):
+                        break
+                    elif (o := code[ind]) == '[':
+                        match += 1
+                    elif o == ']':
+                        match -= 1
+        elif char == ']':
+            ind = lst.pop() - 1
+
+        ind += 1
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        with open(sys.argv[1]) as file:
+            data = file.read()
+            run(data)
