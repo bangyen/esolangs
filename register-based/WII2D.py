f6b50fd5ddcce7091e38ff5796855d9673fa52ab
bangyen <bangyen99@gmail.com>
2021-06-28 18:59:11 -0400

    cleaned up interpreter

--- register-based/WII2D.py
+++ register-based/WII2D.py
@@ -1,78 +1,105 @@
 import secrets
+import copy
 import sys
 
 
-def move(cur, arr, string):
-    x = (cur[0] + arr[0]) % len(string)
-    y = (cur[1] + arr[1]) % len(string[x])
-    return x, y
+def init(lst):
+    n = len(lst)
+    m = len(lst[0])
+    lst = [
+        (-1, 0), (1, 0),
+        (0, -1), (0, 1)
+    ]
+
+    def move(x, y, vel):
+        a, b = lst[vel]
+        x = (x + a) % n
+        y = (y + b) % m
+        return x, y
+    return move
 
 
-def dist(x2, y2):
-    def curry(v):
-        x = abs(v[0] - x2)
-        y = abs(v[1] - y2)
-        return x + y
-    return curry
+def close(lst):
+    def start(x, y):
+        def dist(c):
+            a = abs(c[0] - x)
+            b = abs(c[1] - y)
+            return a + b
+        return dist
+
+    pos = []
+
+    for num, val in enumerate(lst):
+        for n, v in enumerate(val):
+            if num and v == '@':
+                pos.append((num, n))
+
+    def find(x, y):
+        arr = copy.deepcopy(pos)
+        arr.sort(key=start(x, y))
+        if (c := (x, y)) in arr:
+            arr.remove(c)
+        return arr[0] if arr else None
+    return find
 
 
-def find(string, sym):
-    lst = []
-    for i in range(len(string)):
-        for j in range(len(string[i])):
-            if i and string[i][j] == sym:
-                lst.append((i, j))
-    return lst
+def update(op, acc):
+    if op.isdigit():
+        return int(op)
+    elif op == '+':
+        return acc + 1
+    elif op == '-':
+        return acc - 1
+    elif op == '*':
+        return acc * 2
+    elif op == '/':
+        return acc // 2
+    elif op == 's':
+        return acc ** 2
+    elif op == '~':
+        print(chr(acc), end='')
+    return acc
 
 
-def closest(cur, lst):
-    lst.remove(cur)
-    lst = sorted(lst, key=dist(*cur))
-    return lst[0] if lst else None
+def run(code):
+    for num, val in enumerate(code):
+        if '!' in val:
+            x, y = num, val.find('!')
+            break
+    else:
+        return
 
+    size = max(len(lst) for lst in code)
+    code = [c.ljust(size) for c in code]
 
-dir_list = [
-    vel := (-1, acc := 0),
-    (1, 0), (0, -1), (0, 1)
-]
+    find = close(code)
+    move = init(code)
+    vel = acc = 0
 
-op_list = [
-    lambda x: x + 1,
-    lambda x: x - 1,
-    lambda x: x * 2,
-    lambda x: x ** 2,
-    lambda x: x // 2
-]
+    while True:
+        if (op := code[x][y]) in '^v<>':
+            vel = '^v<>'.index(op)
+        elif op == '?':
+            vel = secrets.randbelow(4)
+        elif op == '|':
+            if vel % 2:
+                vel -= 1
+            else:
+                vel += 1
+        elif op == '@':
+            if w := find(x, y):
+                x, y = w
+                x -= 1
+                continue
+        elif op == '.':
+            return
+
+        acc = update(op, acc)
+        x, y = move(x, y, vel)
+
 
 if __name__ == '__main__':
-    code = open(sys.argv[1]).readlines()
-    start = find(code, '!')
-    pos = start[0] if len(start) == 1 else None
-
-    option = '^v<>?'
-    direct = '^v<>'
-    math = '+-*s/'
-
-    while pos:
-        op = code[pos[0]][pos[1]]
-        if op in option:
-            if op == '?':
-                op = secrets.choice(direct)
-            vel = dir_list[direct.index(op)]
-        elif op == '|':
-            a, b = vel
-            vel = [-a, -b]
-        elif op == '@':
-            warps = closest(pos, find(code, '@'))
-            if warps:
-                pos = move(warps, [-1, 0], code)
-                continue
-        elif op.isdigit():
-            acc = int(op)
-        elif op in math:
-            acc = op_list[math.index(op)](acc)
-        elif op == '~':
-            print(chr(acc), end='')
-        elif op == '.':
-            break
-        pos = move(pos, vel, code)
+    if len(sys.argv) > 1:
+        with open(sys.argv[1]) as file:
+            data = file.readlines()
+            run(data)
