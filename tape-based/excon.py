e2ebb565ee7ca45674c55f67111e900dfe44e805
bangyen <bangyen99@gmail.com>
2021-06-29 15:24:53 -0400

    moved logic into function, added separate block for file opening

--- tape-based/excon.py
+++ tape-based/excon.py
@@ -1,15 +1,23 @@
 import sys
 
-pool, cell = ([0] * 8, 7)
-code = open(sys.argv[1]).read()
 
-for sym in code:
-    if sym == ':':
-        pool, cell = ([0] * 8, 7)
-    elif sym == '^':
-        pool[cell] ^= 1
-    elif sym == '!':
-        num = ''.join(map(str, pool))
-        print(chr(int(num, 2)), end='')
-    elif sym == '<':
-        cell -= 1
+def run(code):
+    pool, cell = ([0] * 8, 7)
+
+    for sym in code:
+        if sym == ':':
+            pool, cell = ([0] * 8, 7)
+        elif sym == '^':
+            pool[cell] ^= 1
+        elif sym == '!':
+            num = ''.join(map(str, pool))
+            print(chr(int(num, 2)), end='')
+        elif sym == '<':
+            cell -= 1
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        with open(sys.argv[1]) as file:
+            data = file.read()
+            run(data)
