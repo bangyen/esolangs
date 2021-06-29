9761be2365bd22dffd190c260ff9b1d045bce8c9
bangyen <bangyen99@gmail.com>
2021-06-29 15:20:34 -0400

    cleaned up interpreter

--- tape-based/suffolk.py
+++ tape-based/suffolk.py
@@ -1,33 +1,44 @@
 import sys
 
-tape = [0]
-limit = 10
-count = instruct = 0
-pointer = internal = line = 0
 
-if __name__ == '__main__':
-    if len(args := sys.argv) > 2 and args[2].isdigit():
-        limit = int(args[2])
-    code = open(args[1]).read()
+def run(code, limit=10):
+    tape = [0]
+    num = ind = 0
+    ptr = acc = 0
+    new = 1
 
-    while count < limit:
-        inst = instruct % len(code)
-        sym = code[inst]
-        if sym == '>':
-            pointer += 1
-            if pointer > len(tape) - 1:
+    while num < limit:
+        if (sym := code[ind]) == '>':
+            ptr += 1
+            if ptr == len(tape):
                 tape.append(0)
         elif sym == '<':
-            internal += tape[pointer]
-            pointer = 0
+            acc += tape[ptr]
+            ptr = 0
         elif sym == '!':
-            tape[pointer] = max(0, tape[pointer] + 1 - internal)
-            pointer = internal = 0
+            val = tape[ptr] + 1 - acc
+            tape[ptr] = max(0, val)
+            ptr = acc = 0
         elif sym == ',':
-            inp = input('\n' * line + 'Input: ')
-            internal = internal + ord(inp[0]) if inp else 0
-            line = 1
-        elif sym == '.':
-            print(chr(max(0, internal - 1)), end='')
-        instruct += 1
-        count = instruct // len(code)
+            inp = input('\nInput: '[new:])
+            acc = acc + ord(inp[0]) if inp else 0
+            new = 1
+        elif sym == '.' and acc:
+            val = chr(acc - 1)
+            print(val, end='')
+            new = 0
+
+        ind += 1
+        if ind == len(code):
+            ind = 0
+            num += 1
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        with open(sys.argv[1]) as file:
+            data = file.read()
+            if len(sys.argv) > 2:
+                run(data, int(sys.argv[2]))
+            else:
+                run(data)
