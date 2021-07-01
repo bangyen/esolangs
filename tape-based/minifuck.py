dcb76bf1f947605bf9a79f0a6034ce3d2492ef70
bangyen <bangyen99@gmail.com>
2021-07-01 01:22:22 -0400

    changed file open method, cleaned up input command

--- tape-based/minifuck.py
+++ tape-based/minifuck.py
@@ -4,12 +4,12 @@ import sys
 def run(code):
     tape = [0] * 8
     ptr = ind = 0
-    inp = False
+    inp = 1
 
     while ind < len(code):
         ins = code[ind]
-        if ins == '<':
-            ptr = max(ptr - 1, 0)
+        if ins == '<' and ptr:
+            ptr -= 1
         elif ins in '.[':
             ptr += 1
             if ptr + 1 >= len(tape):
@@ -17,15 +17,15 @@ def run(code):
             tape[ptr] ^= 1
 
             if ins == '.':
-                if n := int(''.join(str(k) for k in tape[:8]), 2):
+                lst = map(str, tape[:8])
+                if n := int(''.join(lst), 2):
                     print(chr(n), end='')
-                    inp = True
+                    inp = 0
                 else:
-                    val = input('\n' * inp + 'Input: ')
-                    val = '0' * 8 + bin(ord((val + '\0')[0]))[2:]
-                    tape = [int(k) for k in val[-8:]] + tape[8:]
-
-                    inp = False
+                    val = input('\nInput: '[inp:])
+                    val = bin(ord(val[0]))[2:].zfill(8)
+                    tape = [*map(int, val)] + tape[8:]
+                    inp = 1
             else:
                 if not tape[ptr]:
                     tape[ptr + 1] ^= 1
@@ -35,8 +35,7 @@ def run(code):
 
 
 if __name__ == '__main__':
-    f = open(sys.argv[1])
-    data = f.read()
-    f.close()
-
-    run(data)
+    if len(sys.argv) > 1:
+        with open(sys.argv[1]) as file:
+            data = file.read()
+            run(data)
