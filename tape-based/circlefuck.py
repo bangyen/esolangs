846f4f3df3a988d8c252b24d5bb4699189fac7d5
bangyen <bangyen99@gmail.com>
2021-07-01 00:40:32 -0400

    moved more logic into find(), changed regex in parse()

--- tape-based/circlefuck.py
+++ tape-based/circlefuck.py
@@ -3,34 +3,49 @@ import re
 
 
 def parse(code):
-    reg = re.compile(r'\\(?:\d\d\d|[\dA-F])')
-    for s in reg.findall(code):
+    reg = (r'\\(?:\d\d\d|'
+           r'[\dA-F](?:$|[^\d]))')
+    exp = r'((^|[^\\]) |\\( )|(\\)o)'
+
+    for s in re.findall(reg, code):
         if len(s) == 4:
-            new = (oct(int(s[1:]))[2:]
-                   .rjust(3, '0'))
+            new = (oct(int(s[1:]))
+                   [2:].zfill(3))
         else:
             new = f'x0{s[1:]}'
         code = code.replace(s, f'\\{new}')
 
-    code = (''.join(c for c in code if ord(c) < 127)
-              .replace('\\ ', '\200')
-              .replace('\\o', '\\'))
-    code = (''.join(c for c in code if ord(c) > 32)
-              .replace('\200', ' '))
+    code = re.sub(exp, r'\2\3\4', code)
+    code = ''.join(c for c in code if
+                   31 < ord(c) < 127)
     code = (bytes(code, 'utf-8')
             .decode('unicode_escape'))
 
     return [ord(c) for c in code]
 
 
-def find(code, ind, mode):
+def find(code, ind, ptr):
+    char = chr(code[ind])
+    if char == '[':
+        if code[ptr]:
+            return ind
+        mode = 1
+    else:
+        if not code[ptr]:
+            return ind
+        mode = -1
+
     match = mode
     start = ind
+    num = len(code)
+
     while match:
-        ind = (ind + mode) % len(code)
+        ind = (ind + mode) % num
         if ind == start:
             return -1
-        if (sym := chr(code[ind])) == '[':
+
+        sym = chr(code[ind])
+        if sym == '[':
             match += 1
         elif sym == ']':
             match -= 1
@@ -40,7 +55,7 @@ def find(code, ind, mode):
 def run(code):
     code = parse(code)
     ind = ptr = 0
-    new = False
+    new = 1
 
     while True:
         if (char := chr(code[ind])) == '>':
@@ -52,21 +67,15 @@ def run(code):
         elif char == '-':
             code[ptr] = (code[ptr] - 1) % 256
         elif char == '.':
-            print(chr(code[ptr]), end='')
-            new = True
+            val = chr(code[ptr])
+            print(val, end='')
+            new = 0
         elif char == ',':
-            val = input('\n' * new + 'Input: ')
-            new = False
-            if val:
-                code[ptr] = ord(val[0])
-        elif char == '[':
-            if not code[ptr]:
-                ind = find(code, ind, 1)
-            if ind == -1:
-                return
-        elif char == ']':
-            if code[ptr]:
-                ind = find(code, ind, -1)
+            val = input('\nInput: '[new:])
+            code[ptr] = ord(val[0])
+            new = 1
+        elif char in '[]':
+            ind = find(code, ind, ptr)
             if ind == -1:
                 return
         elif char == '@':
