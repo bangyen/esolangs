8ec52bd89df34d9ac639011bc131423268e64b68
bangyen <bangyen99@gmail.com>
2021-07-02 16:18:22 -0400

    replaced old parsing with regex

--- register-based/movesum.py
+++ register-based/movesum.py
@@ -1,46 +1,56 @@
 import sys
 import re
 
-count = pointer = new = 0
-cells = {k: 0 for k in range(5)}
+
+def run(code):
+    reg = re.compile(r'(\d+) *= *(\d+)')
+    arr = {k: 0 for k in range(5)}
+    num = ind = 0
+    new = 1
+
+    for m in reg.finditer(code[0]):
+        x, y = m[1], m[2]
+        if x == '42':
+            x = input('Key: ')
+        elif y == '42':
+            y = input('Value: ')
+            y = y if y else 0
+        arr[int(x)] = int(y)
+
+    code = code[1:]
+    while num < 2:
+        copy = arr.copy()
+        expr = (r'(move *(-?\d+)'
+                r' *(-?\d+)|sum)')
+
+        if m := re.search(expr, code[ind]):
+            if m[1] == 'sum':
+                arr[0] = arr[1]
+                for k in range(2, 5):
+                    arr[0] += arr[k]
+            else:
+                if m[2].isdigit():
+                    x = int(m[2])
+                    n = arr.get(x, 0)
+
+                    if m[3].isdigit():
+                        y = int(m[3])
+                        arr[y] = n
+                    else:
+                        print(n, end=' ')
+                        new = 0
+                elif m[3].isdigit():
+                    y = int(m[3])
+                    n = input('\nInput: '[new:])
+                    arr[y] = int(n) if n else 0
+                    new = 1
+
+        num = (num + 1) * (arr == copy)
+        ind = (ind + 1) % len(code)
+
 
 if __name__ == '__main__':
-    first, *code = re.sub(' *\n *', '\n', open(sys.argv[1]).read()).split('\n')
-
-    if re.match('( *([0-9]+=-?[0-9]+) *)+', first):
-        first = [
-            [(int(n), '')[n == '42'] for n in eq.split('=')]
-            for eq in first.split()
-        ]
-    else:
-        first = code = []
-
-    for x, y in first:
-        while not str(x).isdigit():
-            x = input('Key: ')
-        while not str(y).isdigit():
-            y = input('Value: ')
-        cells[int(x)] = int(y)
-
-    while code and count < 2:
-        copy = cells.copy()
-        func, one, two, *rest = f'{code[pointer]} | | |'.split()
-        if func == 'move':
-            if not (one.isdigit() or two.isdigit()):
-                count += 1
-                continue
-            if '-' in one:
-                while not one.isdigit():
-                    one = input('\n' * new + 'Input: ')
-                    new = True
-                cells[int(two)] = int(one)
-            else:
-                if '-' in two:
-                    print(f'{cells[int(one)]} ', end='')
-                else:
-                    val = cells[int(one)] if int(one) in cells else 0
-                    cells[int(two) if two.isdigit() else -1] = val
-        elif func == 'sum':
-            cells[0] = sum(cells[k] for k in range(1, 5))
-        count = (count + 1) * (cells == copy)
-        pointer = (pointer + 1) % len(code)
+    if len(sys.argv) > 1:
+        with open(sys.argv[1]) as file:
+            data = file.readlines()
+            run(data)
