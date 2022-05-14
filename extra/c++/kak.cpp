573371515931b8c93c582b2bb6289e017eba0eb9
Bangyen Pham <66754151+bangyen@users.noreply.github.com>
2022-05-14 09:31:56 -0400

    Create kak.cpp

---
+++ extra/c++/kak.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+int main(int argc, char* argv[]) {
+    if (argc == 1)
+        return EXIT_FAILURE;
+
+    std::ifstream file(argv[1]);
+    std::vector<bool> tape = { 0 };
+    size_t ptr = 0;
+    char c;
+
+    if (!file.is_open())
+        return EXIT_FAILURE;
+
+    do {
+        while (file.get(c)) {
+            if (c == '!') {
+                if (++ptr == tape.size())
+                    tape.push_back(0);
+                tape[ptr] = !tape[ptr];
+            } else if (c == '?' && !tape[ptr]) {
+                file.get(c);
+
+                while (c != '!' && c != '?' && c != '<') {
+                    if (!file.get(c))
+                        return EXIT_FAILURE;
+                }
+            } else if (c == '<' && ptr) {
+                ptr--;
+            }
+        }
+
+        for (bool b : tape)
+            std::cout << b;
+
+        file.clear();
+        file.seekg(0);
+    } while (tape[ptr]);
+
+    file.close();
+    return EXIT_SUCCESS;
+}
