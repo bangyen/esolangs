35d9d1885237a3c1a1c4356c24dc9c7cb8541564
Bangyen Pham <bangyen99@gmail.com>
2026-08-06 00:14:05 -0400

    refactor: convert modulous to a dispatch table of opcode handlers
    Replace the if/elif opcode chain with a State dataclass and a dispatch
    dict keyed by the first token, with each handler as a focused function.
    The main loop is now just table lookup plus the variable-arithmetic
    fallback. Same behavior (19 tests, 100% coverage).

--- src/esolangs/interpreters/stack_based/modulous.py
+++ src/esolangs/interpreters/stack_based/modulous.py
@@ -1,83 +1,148 @@
 import re
 import secrets
 import sys
+from dataclasses import dataclass, field
+from typing import Callable, List, Optional
+
+
+@dataclass
+class State:
+    stk: list = field(default_factory=list)
+    var: dict = field(default_factory=dict)
+    new: int = 1
+    ind: int = 0
+
+
+def _jmp(state: State, mod: str, arg: List[str]) -> Optional[str]:
+    cond = True
+    val = state.stk[-1] if state.stk else 0
+
+    if "NIF" in mod:
+        cond = val != int(arg[-1])
+    elif "IF" in mod:
+        cond = val == int(arg[-1])
+
+    if cond:
+        if arg[1] == "F":
+            state.ind += int(arg[2]) - 1
+        else:
+            state.ind -= int(arg[2]) + 1
+    return None
+
+
+def _add(state: State, mod: str, arg: List[str]) -> Optional[str]:
+    state.stk[-1] += int(arg[1])
+    return None
+
+
+def _sub(state: State, mod: str, arg: List[str]) -> Optional[str]:
+    state.stk[-1] -= int(arg[1])
+    return None
+
+
+def _rst(state: State, mod: str, arg: List[str]) -> Optional[str]:
+    state.ind = -1
+    return None
+
+
+def _psh(state: State, mod: str, arg: List[str]) -> Optional[str]:
+    if "INT" in mod:
+        state.stk.append(int(arg[2]))
+    elif "STR" in mod:
+        m = mod.split('"')[1]
+        state.stk += [ord(c) for c in m][::-1]
+    elif "VAR" in mod:
+        state.var[arg[1]] = state.stk[-1]
+    return None
+
+
+def _pop(state: State, mod: str, arg: List[str]) -> Optional[str]:
+    state.stk.pop()
+    return None
+
+
+def _swp(state: State, mod: str, arg: List[str]) -> Optional[str]:
+    state.stk.append(state.stk.pop(-2))
+    return None
+
+
+def _prt(state: State, mod: str, arg: List[str]) -> Optional[str]:
+    n = state.var[arg[1]] if "VAR" in mod else state.stk.pop()
+
+    if "INT" in mod:
+        print(n, end="")
+    else:
+        print(chr(n), end="")
+    state.new = 0
+    return None
+
+
+def _inp(state: State, mod: str, arg: List[str]) -> Optional[str]:
+    input_str: str = input("\nInput: "[state.new :])
+    state.new = 1
+
+    if "INT" in mod and input_str:
+        state.stk.append(int(input_str))
+    else:
+        state.stk += [ord(c) for c in str(input_str)][::-1]
+    return None
+
+
+def _end(state: State, mod: str, arg: List[str]) -> Optional[str]:
+    return "halt"
+
+
+def _dup(state: State, mod: str, arg: List[str]) -> Optional[str]:
+    state.stk.append(state.stk[-1])
+    return None
+
+
+def _rnd(state: State, mod: str, arg: List[str]) -> Optional[str]:
+    state.stk.append(secrets.randbelow(int(arg[1])))
+    return None
+
+
+def _var_arith(state: State, mod: str) -> None:
+    if "+" in mod:
+        lhs, rhs = mod.split("+")
+        state.var[lhs] += int(rhs)
+    elif "-" in mod:
+        lhs, rhs = mod.split("-")
+        state.var[lhs] -= int(rhs)
+
+
+_DISPATCH: dict[str, Callable[[State, str, List[str]], Optional[str]]] = {
+    "JMP": _jmp,
+    "ADD": _add,
+    "SUB": _sub,
+    "RST": _rst,
+    "PSH": _psh,
+    "POP": _pop,
+    "SWP": _swp,
+    "PRT": _prt,
+    "INP": _inp,
+    "END": _end,
+    "DUP": _dup,
+    "RND": _rnd,
+}
 
 
 def run(code):
     reg = re.compile(r'\[([^\[\]\"]*("[^"]*")?)]')
     code = [k[0] for k in reg.findall(code)]
-    var = {f"VAR{k}": 0 for k in range(1, 5)}
+    state = State(var={f"VAR{k}": 0 for k in range(1, 5)})
 
-    stk: list = []
-    new = 1
-    ind = 0
-
-    while ind < len(code):
-        mod = code[ind]
+    while state.ind < len(code):
+        mod = code[state.ind]
         arg = mod.split()
-        ind += 1
+        state.ind += 1
 
-        if "JMP" in mod:
-            cond = True
-            val = stk[-1] if stk else 0
-
-            if "NIF" in mod:
-                cond = val != int(arg[-1])
-            elif "IF" in mod:
-                cond = val == int(arg[-1])
-
-            if cond:
-                if arg[1] == "F":
-                    ind += int(arg[2]) - 1
-                else:
-                    ind -= int(arg[2]) + 1
-        elif "ADD" in mod:
-            stk[-1] += int(arg[1])
-        elif "SUB" in mod:
-            stk[-1] -= int(arg[1])
-        elif mod == "RST":
-            ind = -1
-        elif "PSH" in mod:
-            if "INT" in mod:
-                stk.append(int(arg[2]))
-            elif "STR" in mod:
-                m = mod.split('"')[1]
-                stk += [ord(c) for c in m][::-1]
-            elif "VAR" in mod:
-                var[arg[1]] = stk[-1]
-        elif mod == "POP":
-            stk.pop()
-        elif mod == "SWP":
-            stk.append(stk.pop(-2))
-        elif "PRT" in mod:
-            n = var[arg[1]] if "VAR" in mod else stk.pop()
-
-            if "INT" in mod:
-                print(n, end="")
-            else:
-                print(chr(n), end="")
-            new = 0
-        elif "INP" in mod:
-            input_str: str = input("\nInput: "[new:])
-            new = 1
-
-            if "INT" in mod and input_str:
-                stk.append(int(input_str))
-            else:
-                stk += [ord(c) for c in str(input_str)][::-1]
-        elif mod == "END":
-            return
-        elif mod == "DUP":
-            stk.append(stk[-1])
-        elif "RND" in mod:
-            n = secrets.randbelow(int(arg[1]))
-            stk.append(n)
-        elif "+" in mod:
-            arg = mod.split("+")
-            var[arg[0]] += int(arg[1])
-        elif "-" in mod:
-            arg = mod.split("-")
-            var[arg[0]] -= int(arg[1])
+        handler = _DISPATCH.get(arg[0])
+        if handler is not None:
+            if handler(state, mod, arg) == "halt":
+                return
+        elif "+" in mod or "-" in mod:
+            _var_arith(state, mod)
 
 
 if __name__ == "__main__":
