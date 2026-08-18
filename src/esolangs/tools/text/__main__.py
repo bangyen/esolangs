dc7e9ed477d089986aeafa613bcc180decd0c746
Bangyen Pham <bangyen99@gmail.com>
2026-08-18 10:20:35 -0400

    tools: black-format text/__main__.py

--- src/esolangs/tools/text/__main__.py
+++ src/esolangs/tools/text/__main__.py
@@ -1,10 +1,9 @@
 """Run the text generators for a sample text.
 
-    python -m esolangs.tools.text "Hello, World!"
+python -m esolangs.tools.text "Hello, World!"
 """
 
 from esolangs.tools.text import main
-
 
 if __name__ == "__main__":
     main()
